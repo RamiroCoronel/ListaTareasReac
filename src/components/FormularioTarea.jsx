4b9553358d8d7c2851017fc142f139e71276d733
Ramiro <ramirocoronelcardenas@gmail.com>
2023-01-12 15:49:05 -0300

    guardamos las tareas en el localstorage

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Form, FormControl, FormGroup, ListGroup } from 'react-bootstrap';
 import ListaTarea from './ListaTarea';
 
 const FormularioTarea = () => {
-  const [tarea, setTarea] =  useState(``);
-  const [arregloTareas, setArregloTareas] = useState([]);
+  //buscar las tareas del local storage si es que existe
+  const tareasLocalstorage = JSON.parse(localStorage.getItem(`listaTareas`)) || [];
+  const [tarea, setTarea] =  useState("");
+  const [arregloTareas, setArregloTareas] = useState(tareasLocalstorage);
+
+//ciclo de vida de un componente
+useEffect(()=>{
+  //console.log(`prueba de ciclo de vida`);
+  //guardar el arreglo de las tareas en local storage
+  localStorage.setItem(`listaTareas`, JSON.stringify(arregloTareas));
+},[arregloTareas])
+
+
   const handleSubmit = (e)=>{
     e.preventDefault();
 //no se puede utilizar el push en 1 state
 setArregloTareas([...arregloTareas, tarea]);
 //limpiar el input
 setTarea(``);
-
-
-  }
-
-    return (
+};
+const borrarTarea =  (nombre)=>{
+  let arregloModificado = arregloTareas.filter((item)=>item !== nombre);
+//actualizar el state
+setArregloTareas(arregloModificado);
+}
+    
+      return (
       <>
      <Form className='my-5' onSubmit={handleSubmit}>
         <FormGroup className='mb-3 d-flex' controlId='formTarea'>
@@ -23,7 +37,7 @@ setTarea(``);
           <Button className='primary' type='submit'>Enviar</Button>
         </FormGroup>
       </Form>
-      <ListaTarea arregloTareas={arregloTareas} ></ListaTarea>
+      <ListaTarea arregloTareas={arregloTareas} borrarTarea={borrarTarea} ></ListaTarea>
       </>
    
     );
